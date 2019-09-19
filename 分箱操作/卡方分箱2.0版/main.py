5e623316d0479aa2c1c6e07a4fe2bdaf9450a6f7
17681556642 <saixin566@163.com>
2019-09-19 11:02:40 +0800

    新增卡方分箱

---
+++ 分箱操作/卡方分箱2.0版/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from 卡方分箱_改 import *
+from WOE import *
+
+
+#分箱处理
+bins = ChiMerge(temp, 'loan_amnt','result', confidenceVal=3.841, bin=10,sample=None)
+print('=================================================================')
+print('分箱节点列表：\n%s' %bins[1])
+print('===========================分箱结果===============================')
+print(bins[0])
+
+
+#WOE/IV计算
+a=iv_value('./KS_data.csv','loan_amnt',tuple(bins[1][1:-1]),'result')
+print('========================WOE/IV计算结果============================')
+print(a[1])
+print('=================================================================')
+print('总IV值：%0.5f' %a[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
